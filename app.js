e45d6b8a8764934621eefe4b82758d863dc959e0
priti <pritikoli1994k@gmail.com>
2025-10-22 21:12:42 +0530

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Jenkins CI/CD Pipeline!');
+  res.end('Welcome from Jenkins CI/CD Pipeline!');
 });
 
 server.listen(port, () => {
